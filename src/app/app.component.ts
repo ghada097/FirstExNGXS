2b2f7e6207dcfd9a1f2a08f3e132fdda37cc4bdc
ghada097 <66436902+ghada097@users.noreply.github.com>
2022-03-14 12:18:46 +0100

    finishing component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import {Select, Store} from '@ngxs/store';
+import {AddTodo, EmptyTodo} from '../app/store/todo.actions';
+import {TodoState} from '../app/store/todo.state';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'FirstExNGXS';
+  newTodo = '';
+
+  constructor(private readonly store: Store) {}
+
+  @Select(TodoState.getTodoList) todoList$?: Observable<string[]>;
+ 
+  onAddTodo(): void {
+    if (this.newTodo.length > 0) {
+      this.store.dispatch(new AddTodo(this.newTodo));
+    }
+    this.newTodo = '';
+  }
+
+  onEmptyList(): void {
+    this.store.dispatch(new EmptyTodo());
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import {environment} from '../environments/environment';
 import {TodoState} from './store/todo.state';
 import {NgxsLoggerPluginModule} from '@ngxs/logger-plugin';
 import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,6 +16,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
     NgxsModule. forRoot ([TodoState], { 
       developmentMode : !environment.production 
     }),
